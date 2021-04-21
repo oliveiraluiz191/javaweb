1e349bb4ff988ac5e994f5a7bd74a9ec2f732907
oliveiraluiz191 <44095349+oliveiraluiz191@users.noreply.github.com>
2021-04-20 21:58:22 -0300

    Adicionando o projeto agenda

---
+++ agenda/WebContent/scripts/confirmador.js
@@ -0,0 +1,12 @@
+/**
+ * Confirmacao de exclusao de um contato
+ * @author lao
+ * @param idcon
+ */
+
+function confirmar(idcon) {
+	let resposta = confirm("Confirma a exclusão deste contato?");
+	if(resposta === true){		
+		window.location.href = "delete?idcon=" + idcon;
+	}
+}
